d6184c9158946b52dbb5229d874872ec78b154f1
abtinokhovat <abtinokhovat@gmail.com>
2023-11-23 20:34:31 +0330

    feat(event): #3 - added base structures for CoreEvent and other essential data structures
    feat(event): add types that where used frequently
    
    feat(event): complete CoreEvent struct and added essential data structs
    
    fix(event): fix context struct properties and some minor name changes
    
    feat(event): add user trait struct and additional classes

--- event/event.go
+++ event/event.go
@@ -1,5 +1,62 @@
 package event
 
-type Event struct {
-	Ctx Context
+import "time"
+
+type Type string
+
+const (
+	TrackEventType    Type = "track"
+	PageEventType     Type = "page"
+	IdentityEventType Type = "identity"
+	GroupEventType    Type = "group"
+	AliasEventType    Type = "alias"
+	ScreenEventType   Type = "screen"
+)
+
+type CoreEvent struct {
+	MessageID ID
+	Type      Type // track, page , ...
+
+	// Category  string // specific to page event
+	Name string
+
+	Properties *Properties
+
+	Integration *Integrations
+	Ctx         *Context
+
+	SendAt            time.Time
+	ReceivedAt        time.Time
+	OriginalTimeStamp time.Time // TODO: don't know the difference between this and Timestamp example(2023-11-19T12:04:25.271Z)
+	Timestamp         time.Time // 2023-11-19T12:04:25.779Z
+
+	AnonymousID ID
+	UserID      ID
+	GroupID     ID
+	PreviousID  ID
+
+	Event string
+
+	WriteKey string
+	MetaData MetaData // TODO: all fields had ambiguity so did not added yet
+
+	Options *Options
+}
+
+type MetaData struct {
+}
+
+type Options struct {
+	Integrations *Integrations
+	Timestamp    time.Time
+	Ctx          *Context
+	AnonymousID  ID
+	UserID       ID
+	Traits       Traits
+	CustomData   *CustomData
+}
+
+type Integrations struct {
+	All    bool
+	Config Properties // TODO: i don't think this is a proper name for this field but, didn't have a better name on my mind
 }
