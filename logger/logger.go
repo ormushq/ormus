71502e3db873e671663251841b7099095a7dee97
DOO-DEV <nimo.vimo.yank@gmail.com>
2023-11-24 01:38:04 +0330

    change: remove colors from logger

--- logger/logger.go
+++ logger/logger.go
@@ -1,53 +1,27 @@
 package logger
 
 import (
-	"bytes"
 	"context"
-	"fmt"
 	"log/slog"
-	"strconv"
-	"sync"
+	"os"
 )
 
-var L = slog.New(newHandler(nil))
-
-const (
-	reset      = "\033[0m"
-	timeFormat = "[02/01/2006 15:04]"
-
-	lightRed   = 91
-	yellow     = 33
-	cyan       = 36
-	darkGray   = 90
-	lightGreen = 92
-	lightGray  = 37
-	white      = 97
-)
-
-func colorize(colorCode int, level string) string {
-	return fmt.Sprintf("\033[%sm%s%s", strconv.Itoa(colorCode), level, reset)
-}
+var L = slog.New(newLoggerHandler(nil))
 
 type handler struct {
 	h slog.Handler
-	b *bytes.Buffer
-	m *sync.Mutex
 }
 
-func newHandler(opts *slog.HandlerOptions) *handler {
+func newLoggerHandler(opts *slog.HandlerOptions) *handler {
 	if opts == nil {
 		opts = &slog.HandlerOptions{}
 	}
-	b := &bytes.Buffer{}
 
 	return &handler{
-		b: b,
-		h: slog.NewJSONHandler(b, &slog.HandlerOptions{
-			Level:       opts.Level,
-			AddSource:   true,
-			ReplaceAttr: overwriteDefaults(opts.ReplaceAttr),
+		h: slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+			Level:     opts.Level,
+			AddSource: true,
 		}),
-		m: &sync.Mutex{},
 	}
 }
 
@@ -56,69 +30,13 @@ func (h *handler) Enabled(ctx context.Context, level slog.Level) bool {
 }
 
 func (h *handler) WithAttrs(attrs []slog.Attr) slog.Handler {
-	return &handler{h: h.h.WithAttrs(attrs), b: h.b, m: h.m}
+	return &handler{h: h.h.WithAttrs(attrs)}
 }
 
 func (h *handler) WithGroup(name string) slog.Handler {
-	return &handler{h: h.h.WithGroup(name), b: h.b, m: h.m}
+	return &handler{h: h.h.WithGroup(name)}
 }
 
 func (h *handler) Handle(ctx context.Context, r slog.Record) error {
-	level := r.Level.String() + ":"
-
-	switch r.Level {
-	case slog.LevelDebug:
-		level = colorize(darkGray, level)
-	case slog.LevelInfo:
-		level = colorize(cyan, level)
-	case slog.LevelWarn:
-		level = colorize(yellow, level)
-	case slog.LevelError:
-		level = colorize(lightRed, level)
-	}
-
-	result, err := h.appendAttrs(ctx, r)
-	if err != nil {
-		return err
-	}
-
-	fmt.Println(
-		colorize(lightGray, r.Time.Format(timeFormat)),
-		level,
-		colorize(lightGreen, r.Message),
-		colorize(white, string(result)),
-	)
-
-	return nil
-}
-
-func (h *handler) appendAttrs(ctx context.Context, r slog.Record) ([]byte, error) {
-	h.m.Lock()
-	defer func() {
-		h.b.Reset()
-		h.m.Unlock()
-	}()
-
-	if err := h.h.Handle(ctx, r); err != nil {
-		return nil, fmt.Errorf("error when calling inner handler's Handle: %w", err)
-	}
-
-	res := bytes.Trim(h.b.Bytes(), "\n")
-
-	return res, nil
-}
-
-func overwriteDefaults(next func([]string, slog.Attr) slog.Attr) func([]string, slog.Attr) slog.Attr {
-	return func(groups []string, a slog.Attr) slog.Attr {
-		if a.Key == slog.TimeKey ||
-			a.Key == slog.LevelKey ||
-			a.Key == slog.MessageKey {
-			return slog.Attr{}
-		}
-		if next == nil {
-			return a
-		}
-
-		return next(groups, a)
-	}
+	return h.h.Handle(ctx, r)
 }
