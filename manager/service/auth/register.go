949e4e51067cb6c6dd9f540d9b8c19dc193bf6e0
abtinokhovat <abtinokhovat@gmail.com>
2023-12-15 02:34:29 +0330

    fix(manager): add repo check before user creation for avoiding duplicated users

--- manager/service/auth/register.go
+++ manager/service/auth/register.go
@@ -5,18 +5,24 @@ import (
 
 	"github.com/ormushq/ormus/manager/entity"
 	"github.com/ormushq/ormus/param"
+	"github.com/ormushq/ormus/pkg/errmsg"
 	"github.com/ormushq/ormus/pkg/password"
 	"github.com/ormushq/ormus/pkg/richerror"
 )
 
 func (s Service) Register(req param.RegisterRequest) (param.RegisterResponse, error) {
+	// fetch user to check if exists before user creation
+	existing, _ := s.repo.GetUserByEmail(req.Email)
+	if existing != nil {
+		return param.RegisterResponse{}, richerror.New("register").WhitMessage(errmsg.ErrAuthUserExisting)
+	}
+
 	hashedPassword, err := password.HashPassword(req.Password)
 	if err != nil {
-		return param.RegisterResponse{}, richerror.New("register").WhitWarpError(err)
+		return param.RegisterResponse{}, richerror.New("register.hash").WhitWarpError(err)
 	}
 
 	user := entity.User{
-		ID:        "0",
 		CreatedAt: time.Now().UTC(),
 		UpdatedAt: time.Now().UTC(),
 		DeletedAt: nil,
@@ -27,12 +33,11 @@ func (s Service) Register(req param.RegisterRequest) (param.RegisterResponse, er
 
 	createdUser, err := s.repo.Register(user)
 	if err != nil {
-		return param.RegisterResponse{}, richerror.New("register").WhitWarpError(err)
+		return param.RegisterResponse{}, richerror.New("register.repo").WhitWarpError(err)
 	}
 
 	//return create new user
 	return param.RegisterResponse{
 		Email: createdUser.Email,
 	}, nil
-
 }
