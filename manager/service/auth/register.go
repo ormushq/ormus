ce76c677f86465b37e7548dddef2f64ff2b2951b
sina hatami <sinahatami1379@gmail.com>
2023-12-14 19:42:33 +0330

    hash register password

--- manager/service/auth/register.go
+++ manager/service/auth/register.go
@@ -3,18 +3,23 @@ package auth
 import (
 	"github.com/ormushq/ormus/manager/entity"
 	"github.com/ormushq/ormus/param"
+	"github.com/ormushq/ormus/pkg/password"
 	"github.com/ormushq/ormus/pkg/richerror"
 	"time"
 )
 
 func (s Service) Register(req param.RegisterRequest) (param.RegisterResponse, error) {
+	hashedPassword, err := password.HashPassword(req.Password)
+	if err != nil {
+		return param.RegisterResponse{}, richerror.New("register").WhitWarpError(err)
+	}
 	user := entity.User{
 		ID:        "0",
 		CreatedAt: time.Now().UTC(),
 		UpdatedAt: time.Now().UTC(),
 		DeletedAt: nil,
 		Email:     req.Email,
-		Password:  req.Password,
+		Password:  hashedPassword,
 		IsActive:  false,
 	}
 	createdUser, err := s.repo.Register(user)
