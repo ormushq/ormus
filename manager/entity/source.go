2169b8e61b2b784e067990cf1805838089556a11
AmirAtashghah <amiratashghah0@gmail.com>
2023-12-25 02:27:09 +0330

    add base of new source

---
+++ manager/entity/source.go
@@ -0,0 +1,4 @@
+package entity
+
+// TODO: Do we need source object?
+type Source struct{}
---
+++ manager/service/sourceservice/source.go
@@ -0,0 +1,10 @@
+package sourceservice
+
+func (s *Service) CreateSource() {}
+
+func (s *Service) UpdateSource() {}
+
+func (s *Service) DeleteSource() {}
+
+// TODO getsource by id or what?
+//func (s *Service) GetSource() {}
