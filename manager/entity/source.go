de8b02652b153627137be4e74e2e559804392ac5
AmirAtashghah <amiratashghah0@gmail.com>
2023-12-26 17:40:12 +0330

    feat(manager): fix lint error

--- manager/entity/source.go
+++ manager/entity/source.go
@@ -6,13 +6,13 @@ import (
 	"github.com/oklog/ulid/v2"
 )
 
-// TODO: need change feilds
+// TODO: need change feilds.
 type Source struct {
 	WriteKey    ulid.ULID
 	Name        string
 	Description string
-	ProjectId   string
-	OwnerId     string
+	ProjectID   string
+	OwnerID     string
 	Status      bool
 	CreateAt    time.Time
 	UpdateAt    time.Time
