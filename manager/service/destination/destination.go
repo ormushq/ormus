cefddce0f347bbd4de4901b6f94506609d8a85af
Benyamin Mahmoudyan <benyaminmahmoudyan@gmail.com>
2023-12-30 22:43:12 +0330

    feat/add update method to Destination interface

--- manager/service/destination/destination.go
+++ manager/service/destination/destination.go
@@ -1,8 +1,11 @@
 package destination
 
-import "github.com/ormushq/ormus/manager/entity"
+import (
+	"context"
+	"github.com/ormushq/ormus/destination/entity"
+)
 
 type Destination interface {
-	GetIntegrationByWriteKey(writeKey string) (entity.Integration, error)
-	// TODO: we need a function that is responsible for update integration in destination cache db when Integration edited
+	GetIntegrationByWriteKey(ctx context.Context, writeKey string) (entity.Integration, error)
+	UpdateIntegrationByWriteKey(ctx context.Context, writeKey string, integration entity.Integration) error
 }
