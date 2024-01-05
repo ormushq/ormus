622b664b3eea88bfa3a36b45d31aeb4cbb5349e1
Benyamin Mahmoudyan <benyaminmahmoudyan@gmail.com>
2024-01-05 18:29:34 +0330

    refactor: replace entity fields in params with custom struct

--- manager/service/destination/destination.go
+++ manager/service/destination/destination.go
@@ -2,10 +2,11 @@ package destination
 
 import (
 	"context"
-	"github.com/ormushq/ormus/destination/entity"
+
+	"github.com/ormushq/ormus/manager/param"
 )
 
 type Destination interface {
-	GetIntegrationByWriteKey(ctx context.Context, writeKey string) (entity.Integration, error)
-	UpdateIntegrationByWriteKey(ctx context.Context, writeKey string, integration entity.Integration) error
+	GetIntegrationByWriteKey(ctx context.Context, writeKey string) (param.ResponseIntegration, error)
+	UpdateIntegrationByWriteKey(ctx context.Context, writeKey string, integration param.RequestIntegration) error
 }
