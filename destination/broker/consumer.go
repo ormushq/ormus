a961704954638397a268f96c89c6e8f464e0eee5
Benyamin Mahmoudyan <benyaminmahmoudyan@gmail.com>
2024-01-06 00:45:27 +0330

    change: consumer for get core events for layer 6

--- destination/broker/consumer.go
+++ destination/broker/consumer.go
@@ -1,7 +1,13 @@
 package broker
 
+// TODO: implement a hybrid solution (get topic from config file and constructor method)
+
 import "github.com/ormushq/ormus/event"
 
+type ConsumerConfig struct {
+	Topic string `koanf:"consumer_topic"`
+}
+
 type Consumer interface {
-	Consume(topic string) <-chan event.CoreEvent
+	Consume() <-chan event.CoreEvent
 }
