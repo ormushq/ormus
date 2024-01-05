4786973c58088026b7425d987ae67e22d4f7a6c9
AmirAtashghah <amiratashghah0@gmail.com>
2024-01-06 01:01:50 +0330

    fix(manager): fix some bugs

--- manager/delivery/httpserver/sourcehandler/delete.go
+++ manager/delivery/httpserver/sourcehandler/delete.go
@@ -10,12 +10,15 @@ func (h Handler) DeleteSource(ctx echo.Context) error {
 	// get id from param
 	sourceID := ctx.Param("sourceID")
 
-	// TODO validate id and check existen source ?
+	// validate form also check existen
+	if err := h.validateSvc.ValidateIDToDelete(sourceID); err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, EchoErrorMessage(err.Error())) // TODO maybe need change response structure
+	}
 
 	// call delete service method
 	if err := h.sourceSvc.DeleteSource(sourceID); err != nil {
 		return echo.NewHTTPError(http.StatusBadRequest, EchoErrorMessage(err.Error())) // TODO maybe need change response structure
 	}
 
-	return ctx.JSON(http.StatusNoContent, echo.Map{"result": "delete"}) // TODO maybe need change response structure
+	return ctx.JSON(http.StatusNoContent, nil) // TODO maybe need change response structure
 }
