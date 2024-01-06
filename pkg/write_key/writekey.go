62a96a151460c79b5d48fecebff9252aa138b260
AmirAtashghah <amiratashghah0@gmail.com>
2024-01-06 10:29:35 +0330

    fix(manager): remove snyc.pool

--- pkg/write_key/writekey.go
+++ pkg/write_key/writekey.go
@@ -2,34 +2,15 @@ package writekey
 
 import (
 	"crypto/rand"
-	"fmt"
-	"sync"
 	"time"
 
 	"github.com/oklog/ulid/v2"
-	"github.com/ormushq/ormus/logger"
 )
 
-var entropyPool = sync.Pool{
-	New: func() any {
-		entropy := ulid.Monotonic(rand.Reader, 0)
-
-		return entropy
-	},
-}
-
 func newULID() (string, error) {
-	e := entropyPool.Get()
-
-	entropy, ok := e.(*ulid.MonotonicEntropy)
-	if !ok {
-		logger.L().Info("Unable to Convert Interface to ULID")
-
-		return "", fmt.Errorf("unable to Convert Interface to ULID")
-	}
+	entropy := ulid.Monotonic(rand.Reader, 0)
 
 	s := ulid.MustNew(ulid.Timestamp(time.Now()), entropy).String()
-	entropyPool.Put(e)
 
 	return s, nil
 }
