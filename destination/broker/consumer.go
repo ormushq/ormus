da604200e09c6e4a05852c387aa12bf9993d80c1
Benyamin Mahmoudyan <benyaminmahmoudyan@gmail.com>
2024-01-06 08:49:16 +0330

    feat:add Consumer interface

---
+++ destination/broker/consumer.go
@@ -0,0 +1,13 @@
+package broker
+
+
+import "github.com/ormushq/ormus/event"
+
+type ConsumerConfig struct {
+	Topic string `koanf:"consumer_topic"`
+}
+
+// TODO: implement a hybrid solution (get topic from config file and constructor method)
+type Consumer interface {
+	Consume() <-chan event.CoreEvent
+}
