793b95ee2c12e33b2c88f5a42eaba4f2769d0a97
Abtin Okhovat <abtinokhovat@gmail.com>
2024-01-25 23:00:49 +0330

    test(project): add project repo mock

---
+++ manager/mock/projectmock/project_repo_mock.go
@@ -0,0 +1,38 @@
+package projectmock
+
+import (
+	"fmt"
+	"github.com/ormushq/ormus/manager/entity"
+	"time"
+)
+
+const RepoErr = "repository error"
+
+type MockProject struct {
+	projects []entity.Project
+	hasErr   bool
+}
+
+func (m MockProject) Create(name string, email string) (entity.Project, error) {
+	if m.hasErr {
+		return entity.Project{}, fmt.Errorf(RepoErr)
+	}
+
+	project := entity.Project{
+		ID:          "new-id",
+		CreatedAt:   time.Now(),
+		UpdatedAt:   time.Now(),
+		DeletedAt:   nil,
+		Name:        name,
+		Description: "",
+		UserID:      fmt.Sprintf("a user with this email: %s", email),
+	}
+
+	m.projects = append(m.projects, project)
+	return project, nil
+}
+
+func New(hasErr bool) MockProject {
+	projects := make([]entity.Project, 10)
+	return MockProject{hasErr: hasErr, projects: projects}
+}
