7d50c10c5a1c70a3b34c950b481d58d760e8b0b6
Abtin Okhovat <abtinokhovat@gmail.com>
2024-01-25 21:36:41 +0330

    feat(project): add create project service

---
+++ manager/service/projectservice/create.go
@@ -0,0 +1,20 @@
+package projectservice
+
+import (
+	"github.com/ormushq/ormus/param"
+	"github.com/ormushq/ormus/pkg/errmsg"
+	"github.com/ormushq/ormus/pkg/richerror"
+)
+
+func (s Service) Create(req param.CreateProjectRequest) (*param.CreateProjectResponse, error) {
+	const op = "projectservice.Create"
+
+	newProject, err := s.repo.Create(req.Name, req.UserID)
+	if err != nil {
+		return nil, richerror.New(op).WhitWarpError(err).WhitMessage(errmsg.ErrSomeThingWentWrong)
+	}
+
+	return &param.CreateProjectResponse{
+		Name: newProject.Name,
+	}, nil
+}
