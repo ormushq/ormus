d9d563a379dc13f9cae23856fbab87895f823352
Abtin Okhovat <abtinokhovat@gmail.com>
2024-01-25 22:06:28 +0330

    feat(project): add isvalid method for user email

---
+++ manager/service/userservice/isvalid.go
@@ -0,0 +1,20 @@
+package userservice
+
+import (
+	"github.com/ormushq/ormus/pkg/errmsg"
+	"github.com/ormushq/ormus/pkg/richerror"
+)
+
+func (s Service) IsUserIDValid(email string) (bool, error) {
+	const op = "userservice.IsUserIDValid"
+
+	user, rErr := s.repo.GetUserByEmail(email)
+	if rErr != nil {
+		return false, richerror.New(op).WhitWarpError(rErr).WhitMessage(errmsg.ErrSomeThingWentWrong)
+	}
+
+	if user == nil {
+		return false, nil
+	}
+	return true, nil
+}
