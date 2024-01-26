eb66c314aefe12ed99cefa95223a2efd86a89d95
Abtin Okhovat <abtinokhovat@gmail.com>
2024-01-26 17:38:27 +0330

    test(ormus): add project service tests

---
+++ manager/service/projectservice/create_test.go
@@ -0,0 +1,55 @@
+package projectservice_test
+
+import (
+	"fmt"
+	"github.com/ormushq/ormus/manager/mock/projectmock"
+	"github.com/ormushq/ormus/manager/service/projectservice"
+	"github.com/ormushq/ormus/param"
+	"github.com/ormushq/ormus/pkg/errmsg"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestService_Create(t *testing.T) {
+	testCases := []struct {
+		name    string
+		repoErr bool
+		req     param.CreateProjectRequest
+		err     error
+	}{
+		{
+			name: "happy path",
+			req: param.CreateProjectRequest{
+				Name:      "new project",
+				UserEmail: "user@example.com",
+			},
+		},
+		{
+			name:    "repo error",
+			repoErr: true,
+			err:     fmt.Errorf(errmsg.ErrSomeThingWentWrong),
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			// 1. setup
+			repo := projectmock.New(tc.repoErr)
+			svc := projectservice.New(repo)
+
+			// 2. execution
+			newProject, err := svc.Create(tc.req)
+
+			// 3. assertion
+			if tc.err != nil {
+				assert.NotNil(t, err)
+				assert.Equal(t, tc.err.Error(), err.Error())
+				return
+			}
+
+			assert.NoError(t, err)
+			assert.NotEmpty(t, newProject.Name)
+			assert.NotEmpty(t, newProject.ID)
+		})
+	}
+}
