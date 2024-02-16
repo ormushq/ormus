8dfb4ba851545c48f9f8a381c05ad778724d1f2e
AmirAtashghah <125047918+AmirAtashghah@users.noreply.github.com>
2024-02-17 00:03:35 +0330

    fix(manager): Update delete.go

--- manager/service/sourceservice/delete.go
+++ manager/service/sourceservice/delete.go
@@ -1,5 +1,5 @@
 package sourceservice
 
-func (s Service) DeleteSource(id string, userID string) error {
+func (s Service) DeleteSource(id, userID string) error {
 	return s.repo.DeleteSource(id, userID)
 }
