ec3f763f2c78d3c932c151e0b5cf84dd9afc7c2e
Pouria Seyfi <seyfipouria@gmail.com>
2024-04-17 03:04:35 +0330

    feat(destination): add debug mode to config

--- destination/dconfig/config.go
+++ destination/dconfig/config.go
@@ -1,6 +1,7 @@
 package dconfig
 
 type Config struct {
+	DebugMode                     bool                          `koanf:"debug_mode"`
 	RabbitMQTaskManagerConnection RabbitMQTaskManagerConnection `koanf:"rabbitmq_task_manager_connection"`
 	RabbitMQConsumerConnection    RabbitMQConsumerConnection    `koanf:"rabbitmq_consumer_connection"`
 	ConsumerTopic                 ConsumerTopic                 `koanf:"consumer_topic"`
