4f23374bf0e9890da3fd2544d77e0d4b4ee69386
Mohsen Haghgoo Darya Kenari <mohsen.hag@mtnirancell.ir>
2024-05-22 15:25:30 +0330

    fix(ormus): Fix lint

--- pkg/channel/adapter/simple/adapter.go
+++ pkg/channel/adapter/simple/adapter.go
@@ -23,6 +23,7 @@ func New(done <-chan bool, wg *sync.WaitGroup) *ChannelAdapter {
 
 func (ca *ChannelAdapter) NewChannel(name string, mode channel.Mode, bufferSize, numberInstants, maxRetryPolicy int) error {
 	ca.channels[name] = newChannel(ca.done, ca.wg, mode, bufferSize, numberInstants, maxRetryPolicy)
+
 	return nil
 }
 
