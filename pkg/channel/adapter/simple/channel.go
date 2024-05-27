45bcb32b34d949ffaa6dc61d5c2eda3a3d2c0b53
Mohsen Haghgoo Darya Kenari <mohsen.hag@mtnirancell.ir>
2024-05-27 13:20:54 +0330

    fix(ormus): some update

--- pkg/channel/adapter/simple/channel.go
+++ pkg/channel/adapter/simple/channel.go
@@ -1,7 +1,7 @@
 package simple
 
 import (
-	"fmt"
+	"github.com/ormushq/ormus/logger"
 	"github.com/ormushq/ormus/pkg/channel"
 	"sync"
 	"time"
@@ -58,7 +58,7 @@ func (sc simpleChannel) startConsume() {
 
 					return
 				case msg := <-sc.inputChannel:
-					fmt.Println("New message received in simple/adapter.go ca.inputChannel", msg)
+					logger.L().Debug("New message received in simple/adapter.go ca.inputChannel")
 					sc.startDelivery(msg)
 				}
 			}
