32ddd6da111466c5029a9387c3fb36c010b0cb44
mehdi <lmehdirezl@gmail.com>
2024-05-30 19:18:26 +0330

    feat(source): add scylladb repo

--- cmd/source/main.go
+++ cmd/source/main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"fmt"
+	"github.com/gocql/gocql"
+	"github.com/ormushq/ormus/adapter/scylladb"
 	"github.com/ormushq/ormus/config"
 	"github.com/ormushq/ormus/source/delivery/httpserver"
-	"github.com/ormushq/ormus/source/mock"
+	"github.com/ormushq/ormus/source/repository/scylladb/event"
 	"github.com/ormushq/ormus/source/service/eventservice"
 	"github.com/ormushq/ormus/source/validator/eventvalidator"
+	"log/slog"
+	"time"
 )
 
 func main() {
@@ -20,10 +25,26 @@ func main() {
 func setupServices(cfg config.Config) (eventSvc eventservice.Service, eventVld eventvalidator.Validator) {
 
 	// TODO: add event Repo syclladb
-	eventRepo := mock.NewMockRepository(false)
+	//eventRepo := mock.NewMockRepository(false)
 
-	eventSvc = eventservice.New(&eventRepo)
-	eventVld = eventvalidator.New(eventRepo)
+	config := scylladb.Config{
+		Hosts:          []string{"127.0.0.1:9042"},
+		Consistency:    gocql.One,
+		Keyspace:       "your_keyspace",
+		TimeoutCluster: 5 * time.Second,
+		NumRetries:     5,
+		MinRetryDelay:  time.Second,
+		MaxRetryDelay:  10 * time.Second,
+	}
+
+	// Create a new ScyllaDB session
+	session, err := scylladb.New(config)
+	if err != nil {
+		slog.Debug(fmt.Sprintf("Error creating ScyllaDB session", err))
+	}
+	sycllaRepo := event.New(session)
+	eventSvc = eventservice.New(&sycllaRepo)
+	eventVld = eventvalidator.New(sycllaRepo)
 
 	return eventSvc, eventVld
 }
---
+++ source/repository/scylladb/event/insert.go
@@ -0,0 +1,12 @@
+package event
+
+import (
+	"context"
+	"fmt"
+	"github.com/ormushq/ormus/event"
+)
+
+func (d DB) InsertEvent(ctx context.Context, e event.CoreEvent) (event.CoreEvent, error) {
+	d.adapter.Query()
+	return event.CoreEvent{}, fmt.Errorf("dsds")
+}
