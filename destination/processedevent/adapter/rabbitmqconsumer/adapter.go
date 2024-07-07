045998095a9ad72f30889526ef268b84d09002ef
Mohsen Haghgoo <mohsen.ha@aol.com>
2024-07-07 12:06:39 +0330

    fix(ormus): update rabbitmqconsumer to use otel trace.

--- destination/processedevent/adapter/rabbitmqconsumer/adapter.go
+++ destination/processedevent/adapter/rabbitmqconsumer/adapter.go
@@ -1,6 +1,10 @@
 package rabbitmqconsumer
 
 import (
+	"context"
+	"github.com/ormushq/ormus/adapter/otela"
+
+	//"context"
 	"fmt"
 	"log"
 	"log/slog"
@@ -15,16 +19,20 @@ import (
 type Consumer struct {
 	topic            dconfig.ConsumerTopic
 	connectionConfig dconfig.RabbitMQConsumerConnection
+	ctx              context.Context
 }
 
-func New(c dconfig.RabbitMQConsumerConnection, topic dconfig.ConsumerTopic) *Consumer {
+func New(ctx context.Context, c dconfig.RabbitMQConsumerConnection, topic dconfig.ConsumerTopic) *Consumer {
 	return &Consumer{
+		ctx:              ctx,
 		connectionConfig: c,
 		topic:            topic,
 	}
 }
 
 func (c *Consumer) Consume(done <-chan bool, wg *sync.WaitGroup) (<-chan event.ProcessedEvent, error) {
+	tracer := otela.NewTracer("rabbitmqconsumer")
+
 	// Start a goroutine to handle incoming messages
 	// todo get size of channel from configs.
 	channelSize := 100
@@ -34,6 +42,7 @@ func (c *Consumer) Consume(done <-chan bool, wg *sync.WaitGroup) (<-chan event.P
 	go func() {
 		defer wg.Done()
 		defer close(events)
+		_, span := tracer.Start(c.ctx, "rabbitmqconsumer@consume")
 
 		conn, err := amqp.Dial(fmt.Sprintf("amqp://%s:%s@%s:%d/", c.connectionConfig.User, c.connectionConfig.Password, c.connectionConfig.Host, c.connectionConfig.Port))
 		failOnError(err, "Failed to connect to RabbitMQ")
@@ -41,6 +50,7 @@ func (c *Consumer) Consume(done <-chan bool, wg *sync.WaitGroup) (<-chan event.P
 			err = conn.Close()
 			failOnError(err, "Failed to close RabbitMQ connection")
 		}(conn)
+		span.AddEvent("connection-established")
 
 		ch, err := conn.Channel()
 		failOnError(err, "Failed to open a channel")
@@ -48,6 +58,7 @@ func (c *Consumer) Consume(done <-chan bool, wg *sync.WaitGroup) (<-chan event.P
 			err = ch.Close()
 			failOnError(err, "Failed to close channel")
 		}(ch)
+		span.AddEvent("channel-established")
 
 		err = ch.ExchangeDeclare(
 			"processed-events-exchange", // name
@@ -59,6 +70,7 @@ func (c *Consumer) Consume(done <-chan bool, wg *sync.WaitGroup) (<-chan event.P
 			nil,                         // arguments
 		)
 		failOnError(err, "Failed to declare an exchange")
+		span.AddEvent("exchange-declare")
 
 		q, err := ch.QueueDeclare(
 			"",    // name
@@ -69,6 +81,7 @@ func (c *Consumer) Consume(done <-chan bool, wg *sync.WaitGroup) (<-chan event.P
 			nil,   // arguments
 		)
 		failOnError(err, "Failed to declare a queue")
+		span.AddEvent("queue-declare")
 
 		err = ch.QueueBind(
 			q.Name,                      // queue name
@@ -77,6 +90,7 @@ func (c *Consumer) Consume(done <-chan bool, wg *sync.WaitGroup) (<-chan event.P
 			false,
 			nil)
 		failOnError(err, "Failed to bind a queue")
+		span.AddEvent("queue-bind")
 
 		msgs, err := ch.Consume(
 			q.Name, // queue
@@ -88,27 +102,40 @@ func (c *Consumer) Consume(done <-chan bool, wg *sync.WaitGroup) (<-chan event.P
 			nil,    // arguments
 		)
 		failOnError(err, "failed to consume")
+		span.AddEvent("consume-started")
+		span.End()
 
 		for {
 			select {
 			case msg := <-msgs:
-				if len(msg.Body) == 0 {
-					break
-				}
-				// Acknowledge the message
-				err := msg.Ack(false)
-				if err != nil {
-					slog.Error(fmt.Sprintf("Failed to acknowledge message: %v", err))
-				}
+				wg.Add(1)
+				go func() {
+					defer wg.Done()
+					if len(msg.Body) == 0 {
+						return
+					}
 
-				e, uErr := taskentity.UnmarshalBytesToProcessedEvent(msg.Body)
-				if uErr != nil {
-					slog.Error(fmt.Sprintf("Failed to convert bytes to processed events: %v", uErr))
+					e, uErr := taskentity.UnmarshalBytesToProcessedEvent(msg.Body)
+					if uErr != nil {
+						slog.Error(fmt.Sprintf("Failed to convert bytes to processed events: %v", uErr))
 
-					break
-				}
+						return
+					}
+					ctx := otela.GetContextFromCarrier(e.TracerCarrier)
+					ctx, span = tracer.Start(ctx, "rabbitmqconsumer@StartProccessEvent")
+					defer span.End()
 
-				events <- e
+					span.AddEvent("process-started")
+
+					e.TracerCarrier = otela.GetCarrierFromContext(ctx)
+					events <- e
+
+					// Acknowledge the message
+					err := msg.Ack(false)
+					if err != nil {
+						slog.Error(fmt.Sprintf("Failed to acknowledge message: %v", err))
+					}
+				}()
 
 			case <-done:
 
