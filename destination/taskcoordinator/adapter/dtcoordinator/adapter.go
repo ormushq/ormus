3ab31c77b6fc3b8a5eac8057bc9c45694b1a1a4e
Mohsen Haghgoo <mohsen.ha@aol.com>
2024-07-07 12:08:43 +0330

    fix(destination): update dtcordinator to use otel trace.

--- destination/taskcoordinator/adapter/dtcoordinator/adapter.go
+++ destination/taskcoordinator/adapter/dtcoordinator/adapter.go
@@ -2,6 +2,9 @@ package dtcoordinator
 
 import (
 	"fmt"
+	"github.com/ormushq/ormus/adapter/otela"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/trace"
 	"log/slog"
 	"sync"
 
@@ -26,25 +29,45 @@ func (d DestinationTypeCoordinator) Start(processedEvents <-chan event.Processed
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
+		tracer := otela.NewTracer("dtcoordinator")
+
 		slog.Info("Starting destination type task coordinator.")
 
 		for {
 			select {
 			case pe := <-processedEvents:
+				wg.Add(1)
+				go func() {
+					defer wg.Done()
+					ctx := otela.GetContextFromCarrier(pe.TracerCarrier)
+					ctx, span := tracer.Start(ctx, "dtcoordinator@ProcessEvent")
+					defer span.End()
 
-				taskPublisher, ok := d.TaskPublishers[pe.DestinationType()]
-				if !ok {
-					slog.Error(fmt.Sprintf("Error on finding task manager for %s", pe.DestinationType()))
+					taskPublisher, ok := d.TaskPublishers[pe.DestinationType()]
+					if !ok {
+						span.AddEvent("error-on-get-task-publisher", trace.WithAttributes(
+							attribute.String("destination-type", string(pe.DestinationType())),
+							attribute.String("error-message", "Task manager not found"),
+						))
+						slog.Error(fmt.Sprintf("Error on finding task manager for %s", pe.DestinationType()))
 
-					break
-				}
+						return
+					}
+					span.AddEvent("task-publisher-founded")
 
-				pErr := taskPublisher.Publish(pe)
-				if pErr != nil {
-					slog.Error(fmt.Sprintf("Error on publishing event : %s", pErr))
+					pe.TracerCarrier = otela.GetCarrierFromContext(ctx)
 
-					break
-				}
+					pErr := taskPublisher.Publish(pe)
+					if pErr != nil {
+						span.AddEvent("error-on-publish-event", trace.WithAttributes(
+							attribute.String("error-message", pErr.Error()),
+						))
+						slog.Error(fmt.Sprintf("Error on publishing event : %s", pErr))
+
+						return
+					}
+					span.AddEvent("task-published")
+				}()
 
 			case <-done:
 
