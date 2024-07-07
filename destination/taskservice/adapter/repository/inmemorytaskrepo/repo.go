7e2db1d881f53381242f3906d8eabaa5dd3e74f1
Mohsen Haghgoo <mohsen.ha@aol.com>
2024-07-07 14:11:27 +0330

    fix(destination): update inmemorytaskrepo to use otel trace.

--- destination/taskservice/adapter/repository/inmemorytaskrepo/repo.go
+++ destination/taskservice/adapter/repository/inmemorytaskrepo/repo.go
@@ -1,33 +1,63 @@
 package inmemorytaskrepo
 
 import (
+	"context"
 	"fmt"
+	"github.com/ormushq/ormus/adapter/otela"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/trace"
 
 	"github.com/ormushq/ormus/destination/entity/taskentity"
 	"github.com/ormushq/ormus/destination/taskservice/param"
 )
 
 func (db DB) GetTaskByID(taskID string) (taskentity.Task, error) {
+	return db.GetTaskByIDWithContext(context.Background(), taskID)
+}
+
+func (db DB) GetTaskByIDWithContext(ctx context.Context, taskID string) (taskentity.Task, error) {
+	tracer := otela.NewTracer("inmemorytaskrepo")
+	_, span := tracer.Start(ctx, "inmemorytaskrepo@GetTaskByIDWithContext", trace.WithAttributes(
+		attribute.String("taskID", taskID),
+	))
+	defer span.End()
+
 	task, ok := db.tasks[taskID]
 	if !ok {
+		span.AddEvent("task-not-found")
+
 		return taskentity.Task{}, fmt.Errorf("task not found")
 	}
+	span.AddEvent("task-fetched")
 
 	return *task, nil
 }
 
 func (db DB) UpsertTask(taskID string, request param.UpsertTaskRequest) error {
-	db.mutex.Lock()
-	defer db.mutex.Unlock()
+	return db.UpsertTaskWithContext(context.Background(), taskID, request)
+}
 
+func (db DB) UpsertTaskWithContext(ctx context.Context, taskID string, request param.UpsertTaskRequest) error {
+	tracer := otela.NewTracer("inmemorytaskrepo")
+	_, span := tracer.Start(ctx, "inmemorytaskrepo@UpsertTaskWithContext", trace.WithAttributes(
+		attribute.String("taskID", taskID),
+	))
+	defer span.End()
+
+	db.mutex.Lock()
+	span.AddEvent("db-tasks-locked")
+	defer db.mutex.Unlock()
 	task, ok := db.tasks[taskID]
 	if !ok {
+		span.AddEvent("task-not-exist-try-to-create")
 		// todo create task
 		task = &taskentity.Task{
 			ID: taskID,
 		}
 		db.tasks[taskID] = task
+		span.AddEvent("task-created")
 	}
+
 	task.DeliveryStatus = request.IntegrationDeliveryStatus
 	task.Attempts = request.Attempts
 	task.FailedReason = request.FailedReason
