afeb42f2362ae90ee89b6d16cb98793209314117
Mohsen Haghgoo <mohsen.ha@aol.com>
2024-07-07 12:11:14 +0330

    fix(destination): update fake_processed_event_producer.go to use otel trace.

--- cmd/destination/faker/fake_processed_event_producer.go
+++ cmd/destination/faker/fake_processed_event_producer.go
@@ -4,7 +4,12 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"github.com/ormushq/ormus/adapter/otela"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/trace"
 	"log"
+	"log/slog"
+	"sync"
 	"time"
 
 	"github.com/ormushq/ormus/config"
@@ -23,9 +28,49 @@ func failOnError(err error, msg string) {
 }
 
 func main() {
+	wg := &sync.WaitGroup{}
+	done := make(chan bool)
+
+	//----------------- Setup Logger -----------------//
+
+	fileMaxSizeInMB := 10
+	fileMaxAgeInDays := 30
+
+	cfg := logger.Config{
+		FilePath:         "./destination/logs.json",
+		UseLocalTime:     false,
+		FileMaxSizeInMB:  fileMaxSizeInMB,
+		FileMaxAgeInDays: fileMaxAgeInDays,
+	}
+
+	logLevel := slog.LevelDebug
+
+	opt := slog.HandlerOptions{
+		// todo should level debug be read from config?
+		Level: logLevel,
+	}
+	l := logger.New(cfg, &opt)
+
+	// use slog as default logger.
+	slog.SetDefault(l)
+
+	//----------------- Setup Tracer -----------------//
+	otelcfg := otela.Config{
+		Endpoint:           config.C().Destination.Otel.Endpoint,
+		ServiceName:        config.C().Destination.Otel.ServiceName + "/FakerEventGenerator",
+		EnableMetricExpose: false,
+	}
+
+	err := otela.Configure(wg, done, otelcfg)
+	failOnError(err, "Failed to configure otel")
+
+	tracer := otela.NewTracer("FakerTracer")
+	_, span := tracer.Start(context.Background(), "FakerTracer@FakerEventPreparation")
+
 	rmqConsumerConnConfig := config.C().Destination.RabbitMQConsumerConnection
 	conn, err := amqp.Dial(fmt.Sprintf("amqp://%s:%s@%s:%d/", rmqConsumerConnConfig.User,
 		rmqConsumerConnConfig.Password, rmqConsumerConnConfig.Host, rmqConsumerConnConfig.Port))
+	span.AddEvent("rabbitmq-connection-established")
 
 	defer func(conn *amqp.Connection) {
 		err = conn.Close()
@@ -38,6 +83,7 @@ func main() {
 		err = ch.Close()
 		failOnError(err, "Failed to close channel")
 	}(ch)
+	span.AddEvent("channel-opened")
 
 	err = ch.ExchangeDeclare(
 		"processed-events-exchange", // name
@@ -49,9 +95,7 @@ func main() {
 		nil,                         // arguments
 	)
 	failOnError(err, "Failed to declare an exchange")
-
-	ctx, cancel := context.WithTimeout(context.Background(), timeoutSeconds*time.Second)
-	defer cancel()
+	span.AddEvent("exchange-declared")
 
 	fakeIntegration := entity.Integration{
 		ID:       "10",
@@ -62,10 +106,13 @@ func main() {
 			Slug: "webhook",
 		},
 	}
+
+	ctx, taskSpan := tracer.Start(context.Background(), "FakerTracer@FakerEventPublisher")
 	// generate fake processedEvent
 	pageName := "Home"
 	pe := event.ProcessedEvent{
 		SourceID:          "1",
+		TracerCarrier:     otela.GetCarrierFromContext(ctx),
 		Integration:       fakeIntegration,
 		MessageID:         "1",
 		EventType:         "page",
@@ -81,6 +128,16 @@ func main() {
 	if err != nil {
 		log.Panicf("Error: %e", err)
 	}
+	taskSpan.AddEvent("json-event-created", trace.WithAttributes(
+		attribute.String("source-id", pe.SourceID),
+		attribute.String("event-id", pe.ID()),
+		attribute.String("event-type", string(pe.EventType)),
+	))
+
+	span.AddEvent("message-published")
+
+	taskSpan.End()
+	span.End()
 
 	err = ch.PublishWithContext(ctx,
 		"processed-events-exchange", // exchange
@@ -93,5 +150,9 @@ func main() {
 		})
 	failOnError(err, "Failed to publish a message")
 
-	logger.L().Debug("Publish new processed event.")
+	l.Debug("Publish new processed event.")
+
+	time.Sleep(time.Second * 5)
+	close(done)
+	wg.Wait()
 }
