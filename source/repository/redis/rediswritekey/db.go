bef2c3914ab821cb493df76e592e0fdbebc87513
Amir Bapiri <amirbapiri@gmail.com>
2024-07-11 23:11:48 +0330

    feat(source): Implement Redis repository for writekey validation

--- source/repository/redis/rediswritekey/db.go
+++ source/repository/redis/rediswritekey/db.go
@@ -1,6 +1,15 @@
 package rediswritekey
 
-import "github.com/ormushq/ormus/adapter/redis"
+import (
+	"context"
+	"errors"
+	"github.com/ormushq/ormus/adapter/redis"
+	rds "github.com/redis/go-redis/v9"
+)
+
+const (
+	WriteKeyValidFlag = "valid"
+)
 
 type DB struct {
 	adapter redis.Adapter
@@ -9,4 +18,18 @@ type DB struct {
 // New is Constructor redis DB.
 func New(adapter redis.Adapter) DB {
 	return DB{adapter: adapter}
+}
+
+func (db DB) IsValidWriteKey(ctx context.Context, writeKey string) (bool, error) {
+	val, err := db.adapter.Client().Get(ctx, writeKey).Result()
+	if errors.Is(err, rds.Nil) {
+		// Write key does not exists
+		return false, nil
+	} else if err != nil {
+		// Some other error took place
+		return false, err
+	}
+
+	// write key exists
+	return val == WriteKeyValidFlag, nil
 }
