8684f5577786cb8e35a999129d6c7370cfea2d67
Mohsen Haghgoo <mohsen.hag@mtnirancell.ir>
2024-08-16 11:34:51 +0330

    fix: init cobra project

---
+++ cli/main.go
@@ -0,0 +1,16 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+*/
+package main
+
+import (
+	"github.com/ormushq/ormus/cli/cmd"
+	_ "github.com/ormushq/ormus/cli/cmd/destination"
+	_ "github.com/ormushq/ormus/cli/cmd/project"
+	_ "github.com/ormushq/ormus/cli/cmd/source"
+	_ "github.com/ormushq/ormus/cli/cmd/user"
+)
+
+func main() {
+	cmd.Execute()
+}
